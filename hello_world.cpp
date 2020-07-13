0b416531b2f8024f9728d765f22da714a42de360
jaehjung <jaehjung@AUS-LX-JAEHJUNG.amd.com>
2020-07-13 15:41:19 -0500

    hello_world source code and gitignore file

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+// This program prints string "Hello World!" to the terminal
+// Load library for in/out stream called "iostream"
+#include <iostream>
+
+// main() function called by the system and all the function must be started and ended with {}
+// empty parameter list () and int return data type specified 
+int main(){
+  // Take in the string to the cout (character outputstream)
+  std::cout << "Hello World!\n";
+  // Reutrn 0 at the end if everything works correct
+  return 0;
+}
